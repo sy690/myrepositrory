e8e98ad92322113bbe83a0a3b0acd27c4636b5f8
Sangita <yadavsm754@gmail.com>
2025-08-06 13:08:58 +0530

    terraform-file

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "my-instance" {
 ami = "ami-020cba7c55df1f615"
 instance_type = "t2.micro"
 key_name = "my"
-vpc_security_group_ids = "sg-075e03b30d0894f2a"
+vpc_security_group_ids = ["sg-075e03b30d0894f2a"]
 
 }
