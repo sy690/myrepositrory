33055b63af2cb51a9efe2cd89582d7cfcd135ccb
Sangita <yadavsm754@gmail.com>
2025-08-06 15:48:20 +0530

    terraform-file2

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -1,41 +1,15 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "terraform-bucket-1231234"
-#     key = "state/tarraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
 
-# provider "aws"{
-#     region = "us-east-1"
+
+provider "aws"{
+    region = "us-east-1"
     
-# }
-# resource "aws_instance" "my-instance" {
-# ami = "ami-020cba7c55df1f615"
-# instance_type = "t2.micro"
-# key_name = "my"
-# vpc_security_group_ids = ["sg-075e03b30d0894f2a"]
+}
+resource "aws_instance" "my-instance" {
+ami = "ami-020cba7c55df1f615"
+instance_type = "t2.micro"
+key_name = "my"
+vpc_security_group_ids = ["sg-075e03b30d0894f2a"]
 
-# }
+}
 
 
-# # provider "aws" {
-# #     region = "us-east-1"
-  
-# # }
-# # terraform {
-# #   backend "s3" {
-# #     bucket = "my-terraform-backend-12345"
-# #     key = "project1/env/dev/terraform.tfstate"
-# #     region = "us-east-1"
-    
-# #   }
-# # }
-# # resource "aws_instance" "my-ins" {
-# #     ami = "ami-020cba7c55df1f615"
-# # instance_type = "t2.micro"
-# #  key_name = "my"
-# # vpc_security_group_ids = ["sg-075e03b30d0894f2a"]
-
-  
-# # }
