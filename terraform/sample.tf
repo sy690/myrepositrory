67887f36a97507a85efe0676bc6757e388723390
Sangita <yadavsm754@gmail.com>
2025-08-06 12:47:25 +0530

    terraform-file

---
+++ terraform/sample.tf
@@ -0,0 +1,11 @@
+provider "aws"{
+    region = "us-east-1"
+    
+}
+resource "aws_instance" "my-instance" {
+ami = "ami-020cba7c55df1f615"
+instance_type = "t2.micro"
+key_name = "my.pem"
+vpc_security_group_ids = ["sg-075e03b30d0894f2a"]
+
+}
